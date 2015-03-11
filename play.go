ac626f182faccd9abcea3145d7ab218ebf133eea
indraniel <indraniel@gmail.com>
2015-03-10 23:27:17 -0500

    + more playing around with bleve

--- play.go
+++ play.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/blevesearch/bleve"
+	"github.com/blevesearch/bleve/document"
 	"log"
+	"os"
+	"time"
 )
 
 func main() {
@@ -22,11 +26,61 @@ func main() {
 	search := bleve.NewSearchRequestOptions(query, 100, 0, false)
 	//search := bleve.NewSearchRequest(query)
 	search.Highlight = bleve.NewHighlightWithStyle("ansi")
-	search.Highlight.AddField("Data.Alias")
-	search.Fields = []string{"SubmissionId", "Published", "Data.Alias", "Data.Description"}
+	//	search.Highlight.AddField("Data.Alias")
+	//	search.Fields = []string{"SubmissionId", "Published", "Data.Alias", "Data.Description"}
 	searchResults, err := index.Search(search)
 	if err != nil {
 		log.Fatalln("[2]", err)
 	}
-	fmt.Printf("%v\n", searchResults)
+	//	jsonStr, _ := json.MarshalIndent(searchResults.Hits, "", "    ")
+	//	fmt.Printf("%s\n", jsonStr)
+	ids := make([]string, 0)
+	for _, val := range searchResults.Hits {
+		id := val.ID
+		doc, _ := index.Document(id)
+
+		rv := struct {
+			ID     string                 `json:"id"`
+			Fields map[string]interface{} `json:"fields"`
+		}{
+			ID:     id,
+			Fields: map[string]interface{}{},
+		}
+		for _, field := range doc.Fields {
+			var newval interface{}
+			switch field := field.(type) {
+			case *document.TextField:
+				newval = string(field.Value())
+			case *document.NumericField:
+				n, err := field.Number()
+				if err == nil {
+					newval = n
+				}
+			case *document.DateTimeField:
+				d, err := field.DateTime()
+				if err == nil {
+					newval = d.Format(time.RFC3339Nano)
+				}
+			}
+			existing, existed := rv.Fields[field.Name()]
+			if existed {
+				switch existing := existing.(type) {
+				case []interface{}:
+					rv.Fields[field.Name()] = append(existing, newval)
+				case interface{}:
+					arr := make([]interface{}, 2)
+					arr[0] = existing
+					arr[1] = newval
+					rv.Fields[field.Name()] = arr
+				}
+			} else {
+				rv.Fields[field.Name()] = newval
+			}
+		}
+
+		j2, _ := json.MarshalIndent(rv, "", "    ")
+		fmt.Printf("%s\n", j2)
+		os.Exit(0)
+	}
+	fmt.Println(ids)
 }
