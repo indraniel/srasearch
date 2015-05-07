aaae164327aa9732320f14df6b3bd5f8ac12c61c
indraniel <indraniel@gmail.com>
2015-05-07 09:03:20 -0500

    + further results page work
      - adjust the sidebar formatting to be consistent on both mobile and
        desktop views
      - include more search result stats at the top

--- web/render/functions.go
+++ web/render/functions.go
@@ -14,7 +14,16 @@ func init() {
 		"mapGet":    MapGet,
 		"timeFmt":   TimeFmt,
 		"shortType": ShortType,
+		"add":       Add,
 	}
+}
+
+func Add(args ...int) int {
+	total := 0
+	for _, v := range args {
+		total += v
+	}
+	return total
 }
 
 func ShortType(member string) string {
