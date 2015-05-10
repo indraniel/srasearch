50de578c3d72439d4a916510a8878b873e960799
indraniel <indraniel@gmail.com>
2015-05-10 06:16:07 -0500

    + parse the correct field for the Study

--- sradump/accessions.go
+++ sradump/accessions.go
@@ -48,7 +48,7 @@ func CollectAccessionStats(tarfile string) *map[string]*sra.AccessionRecord {
 			Visibility: record[8],
 			Experiment: record[10],
 			Sample:     record[11],
-			Study:      record[13],
+			Study:      record[12],
 			MD5:        record[16],
 			BioSample:  record[17],
 			BioProject: record[18],
