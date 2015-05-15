6d28936d30641f0d5792fcf5c83ae5322fc2e1da
indraniel <indraniel@gmail.com>
2015-05-15 16:46:42 -0500

    + include a spinner icon on the search button
      - this could nice for long searches

---
+++ web/static/js/spinner-button.js
@@ -0,0 +1,8 @@
+$('#load').on('click', function() {
+    var $this = $(this);
+    $this.button('searching');
+    $this.parents('form#search-form').submit();
+    setTimeout(function() {
+        $this.button('reset');
+    }, 6000);
+});
